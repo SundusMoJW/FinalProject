b30072cbb70f346372bdd7ea3ea386d8cdbf3a04
Sundus Jawwad <sundus_bjw@hotmail.com>
2017-09-17 11:52:41 +0300

    personal data page

--- app/src/main/java/com/twins/osama/finalproject/Activities/Splash.java
+++ app/src/main/java/com/twins/osama/finalproject/Activities/Splash.java
@@ -4,15 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.twins.osama.finalproject.R;
 
 public class Splash extends AppCompatActivity {
 
+    private ImageView imSplash;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+//        imSplash = (ImageView) findViewById(R.id.imsplash);
+//        Glide.with(getApplicationContext()).load(getResources().getDrawable(R.drawable.back_splah)).into(imSplash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
