ef9597fd21551e821461e436a6b2e3c6c7d4424a
Sundus Jawwad <sundus_bjw@hotmail.com>
2017-09-17 13:56:18 +0300

    personal data page

--- app/src/main/java/com/twins/osama/finalproject/Activities/Login.java
+++ app/src/main/java/com/twins/osama/finalproject/Activities/Login.java
@@ -1,27 +1,65 @@
 package com.twins.osama.finalproject.Activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.twins.osama.finalproject.R;
 
-public class Login extends AppCompatActivity {
+import org.json.JSONObject;
 
+import java.util.Map;
+
+public class Login extends AppCompatActivity {
+    TextView LoginPassword;
+    TextView LoginUser;
     private Button login;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        login=(Button)findViewById(R.id.login);
+        LoginUser = (TextView) findViewById(R.id.LoginUser);
+        LoginPassword = (TextView) findViewById(R.id.LoginPassword);
+        login = (Button) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Login.this, MainActivity.class));
                 finish();
+            }
+        });
+        database = FirebaseDatabase.getInstance();
+        Query patient = database.getReference().child("AddPatient");
+        patient.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot data : dataSnapshot.getChildren()) {
+                        JSONObject dt = new JSONObject((Map) data.getValue());
+                        String uRCN = dt.optString("RCN");
+                        String uRRIS_SSN_NO = dt.optString("RRIS_SSN_NO");
+//                        String title = dt.optString("title");
+//                        String body = dt.optString("body");
+                        if (uRCN.equals(LoginUser.getText())&&uRRIS_SSN_NO.equals(LoginPassword.getText())){
+
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
