7e03af2145fafd86883d0946855e4b87ac9d8ac1
Sundus Jawwad <sundus_bjw@hotmail.com>
2017-09-19 14:44:48 +0300

    personal data page

--- app/src/main/java/com/twins/osama/finalproject/Activities/Login.java
+++ app/src/main/java/com/twins/osama/finalproject/Activities/Login.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -26,47 +26,54 @@ import static com.twins.osama.finalproject.Helpar.Const.USER_RCN_LOGIN;
 import static com.twins.osama.finalproject.Helpar.Const.USER_SSN_LOGIN;
 
 public class Login extends AppCompatActivity {
-    TextView LoginPassword;
-    TextView LoginUser;
+    EditText LoginPassword;
+    EditText LoginUser;
     private Button login;
     FirebaseDatabase database;
     SharedPrefUtil sharedPrefUtil;
-
+    String uRRIS_SSN_NO;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        LoginUser = (TextView) findViewById(R.id.LoginUser);
-        LoginPassword = (TextView) findViewById(R.id.LoginPassword);
+        LoginUser = (EditText) findViewById(R.id.LoginUser);
+        LoginPassword = (EditText) findViewById(R.id.LoginPassword);
         login = (Button) findViewById(R.id.login);
         sharedPrefUtil = new SharedPrefUtil(Login.this);
         database = FirebaseDatabase.getInstance();
-        final Query patient = database.getReference().child("AddPatient").orderByChild("RCN")
-                .equalTo(LoginUser.getText().toString().trim());
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                database.getReference().child("AddPatient").orderByChild("RCN")
-                        .equalTo(LoginUser.getText().toString().trim())
-                        .addValueEventListener(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(DataSnapshot dataSnapshot) {
-                                Log.i("aaaa", dataSnapshot.getValue().toString());
+                if(!(LoginUser.getText().toString().isEmpty())&&!(LoginPassword.getText().toString().isEmpty())) {
+                    final Query patient = database.getReference().child("AddPatient").orderByChild("RCN")
+                            .equalTo(LoginUser.getText().toString().trim());
+//                database.getReference().child("AddPatient").orderByChild("RCN")
+//                        .equalTo(LoginUser.getText().toString().trim())
+                    patient.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            Log.i("aaaa", dataSnapshot.getValue().toString());
 
-                                if (dataSnapshot.exists()) {
-//                            for (DataSnapshot data : dataSnapshot.getChildren()) {
-                                    JSONObject dt = new JSONObject((Map) dataSnapshot.getValue());
-//                                JSONObject dt = new JSONObject((Map) dataSnapshot.getChildren().);
+                            if (dataSnapshot.exists()) {
+                                for (DataSnapshot data : dataSnapshot.getChildren()) {
+
+
+//                                        JSONObject   dt = new JSONObject((Map) dataSnapshot.child(dataSnapshot.getKey()).getValue());
+                                    JSONObject dt = new JSONObject((Map) data.getValue());
+//                                    try {
+//                                        JSONObject dt1=dt.getJSONObject(dataSnapshot.child(dataSnapshot.getKey()).toString());
                                     Log.i("aaaa", "//" + dt.toString());
 //                                    Map<String, String> map=(Map<String, String>) dataSnapshot.getValue();
 //                                    Log.i("aaaa", "//"+map.toString());
 //                                    String uRCN=   map.get("RCN");
 //                                    String uRRIS_SSN_NO =   map.get("RRIS_SSN_NO");
                                     String uRCN = dt.optString("RCN");
-                                    String uRRIS_SSN_NO = dt.optString("RRIS_SSN_NO");
+                                    uRRIS_SSN_NO = dt.optString("RRIS_SSN_NO");
 
-                                        Log.i("aaaa", "//" + dt.optString("RCN"));
+                                    Log.i("aaaa", "//" + dt.optString("RCN"));
+
 
 //                        String title = dt.optString("title"
 //);
@@ -80,17 +87,19 @@ public class Login extends AppCompatActivity {
                                         finish();
                                     } else
                                         Toast.makeText(Login.this, "Invalid RCN or Password", Toast.LENGTH_SHORT).show();
-                                } else
-                                    Toast.makeText(Login.this, "This account not found", Toast.LENGTH_SHORT).show();
-                            }
+                                }
+                            } else
+                                Toast.makeText(Login.this, "This account not found", Toast.LENGTH_SHORT).show();
+                        }
+
 //                    }
 
-                            @Override
-                            public void onCancelled(DatabaseError databaseError) {
-
-                            }
-                        });
-
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            Toast.makeText(Login.this, "You must enter your account", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }else Toast.makeText(Login.this, "You must enter your account", Toast.LENGTH_SHORT).show();
             }
         });
 
